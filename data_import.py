743b93cdf677a65408f78c2fce4149bd56a2e56f
hillarywolff <hwolff@uchicago.edu>
2022-04-14 19:56:29 -0500

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -5,5 +5,45 @@ Created on Thu Apr 14 19:20:31 2022
 
 @author: hillarywolff
 """
+import pandas as pd
+import glob
 
-akdjf
+
+PATH = r"/Users/hillarywolff/Desktop/basketball_data"
+all_files = glob.glob(PATH+ "/*.csv")
+
+
+fields = ['game_id', 'data_set', 'team', 'period', 'away_score', 'home_score', 
+          'remaining_time','elapsed', 'event_type', 'points', 'reason', 
+          'result', 'steal', 'type', 'description']
+
+big_ass_df = pd.DataFrame()
+
+df = []
+for f in all_files:
+    csv = pd.read_csv(f, usecols=fields)
+    df.append(csv)
+
+
+big_ass_df = pd.concat(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
