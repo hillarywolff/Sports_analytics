093dcfd3a52ceecd3a108fa2adb5b31fa322c7e9
hillarywolff <hwolff@uchicago.edu>
2022-04-20 20:23:40 -0500

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -35,18 +35,45 @@ df = big_ass_df
 event_types = ['start of period', 'end of period', 'sub', 'timeout', 'unknown']
 
 df = df[df['event_type'].str.contains('|'.join(event_types))==False]
-
 df = df.fillna(0)
-df_blank = df[df['event_type'].str.contains('rebound')]
 
-df_blank = df_blank[df_blank['team']==0]
-i = df[df['team']==0].index
-df = df.drop(i, axis=0)
+
+# create new df where team = 0
+def separate_team(x):
+    if search(r' ', x):
+        return x.split(' ')[0]
+    
+blank_df = df[df['team']==0]
+blank_df = blank_df[blank_df['description']!=0]
+blank_df['description'] = blank_df['description'].str.lower()
+blank_df['team_separate'] = blank_df['description'].apply(separate_team)
+
+# NEED TO REPLACE JUMP BALL TEAM NAME
+
+
+crosswalk = pd.read_csv("/Users/hillarywolff/Desktop/nba_crosswalk.csv")
+team_crosswalk = crosswalk
+team_crosswalk.columns
+
+team_crosswalk['team name'] = team_crosswalk['team name'].str.lower()
+team_crosswalk = team_crosswalk.set_index('team name').T
+team_crosswalk = team_crosswalk.to_dict('list')
+
+blank_df['team'] = blank_df['team_separate'].map(team_crosswalk)
+
+blank_df['team']= blank_df['team'].str.strip()
+
+
+
+# df_blank = df[df['event_type'].str.contains('rebound')]
+
+# df_blank = df_blank[df_blank['team']==0]
+i = df[~df['team']==0]
+i = list(i)
+df = df.drop(df.index[[i]])
 
 # create crosswalk for team name and abbreviations to map over the team col
-def separate_team(x):
-    if search(r'rebound', x):
-        return x.split(' ')[0]
+
 
 
 
