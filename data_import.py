02986bc8c0b0251c80bdd79fa1eb0d6ca637861f
hillarywolff <hwolff@uchicago.edu>
2022-05-02 11:50:43 -0500

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -14,18 +14,7 @@ from re import search
 ####################################################################
 
 def clean_df(df):
-
-    event_types = ['start of period', 'end of period', 'sub', 'timeout', 'unknown', 'ejection', 'nan']
-    df = df[df['event_type'].str.contains('|'.join(event_types))==False]
     
-    
-    type_remove = ['t.foul', 'c.p.foul', 'Free Throw Technical', 'Free Throw Clear Path 1 of 2',
-                   'Free Throw Clear Path 2 of 2', 'Free Throw Flagrant 1 of 1', 
-                   'Free Throw Flagrant 1 of 1', 'Free Throw Flagrant 1 of 2',
-                   'Free Throw Flagrant 1 of 3', 'Free Throw Flagrant 2 of 2',
-                   'Free Throw Flagrant 2 of 3', 'Free Throw Flagrant 3 of 3']
-    
-    df = df[df['type'].str.contains('/'.join(type_remove))==False]
     df = df.fillna(0)
     # create new df where team = 0
     def separate_team(x):
@@ -51,29 +40,23 @@ def clean_df(df):
     blank_df = blank_df.drop('team_separate', axis=1)
     
     
-    ft_df = df[df['event_type'].str.contains('free throw')]
+    ft_df = df[df['event_type'].str.contains('free throw', na=False)]
     
-    ft_df['points'] = np.where((ft_df['result'] == 'made'), 1, 0)
+    ft_df['points'] = np.where((ft_df['result'].str.contains('made')), 1, 0)
     
-    conditions = [
-        df['event_type'].eq('free throw') & df['result'].eq('made') & df['points'].ne(1),
-        df['team'].eq(0)
-    ]
+    df['remove'] = ((df['event_type'].str.contains('free throw') & 
+                      (df['result'].str.contains('made')) & (df['points'].ne(1))| 
+                    (df['team'].eq(0))), 1, 0)
     
-    choices = [1, 1]
-    
-    df['remove'] = np.select(conditions, choices, default=0)
-    
-    df = df[df.remove == 0]
-    # dropped rows where other dfs fixed
+    df = df[df['remove']==0]
     
     df = pd.concat([df, blank_df, ft_df])
     
     df = df.drop('remove', axis=1)
-    # remove unnecessary col
+    #remove unnecessary col
     
-    ################################################################
     ###############################################################
+    ##############################################################
     df['game_id'] = df['game_id'].str.replace('(\D+)', '')
     df['team'] = df['team'].str.replace('(\W)', '')
     df['event_type'] = df['event_type'].replace({'miss':'shot'})
@@ -87,52 +70,66 @@ def clean_df(df):
     
 def create_dummies(df):
     
-    df['team_shift'] = df['team'].shift(1)
-    df['team_t/f'] = df['team'] == df['team_shift']
+    df['team_below'] = df['team'].shift(1)
+    df['team_below_t/f'] = df['team'] == df['team_below']
+    
+    df['team_above'] = df['team'].shift(-1)
+    df['team_above_t/f'] = df['team'] == df['team_above']
 
-    df['quarter_shift'] = df['period'].shift(1)
-    df['quarter_t/f'] = df['period'] == df['quarter_shift']
 
-    df['reason_cat'] = df['reason'].astype('category')#.cat.codes
+    df['quarter_above'] = df['period'].shift(1)
+    df['quarter_above_t/f'] = df['period'] == df['quarter_above']
+    
+    
+    df['quarter_below'] = df['period'].shift(-1)
+    df['quarter_below_t/f'] = df['period'] == df['quarter_below']
+
+    df['reason_above'] = df['reason'].shift(1).astype('category')#.cat.codes
+    df['reason_above_t/f'] = df['reason'] == df['reason_above']
+    
+    df['reason_below'] = df['reason'].shift(-1)
+    df['reason_below_t/f'] = df['reason'] == df['reason_below']
+    
+    df['type_below'] = df['type'].shift(-1)
 
     
-    df['second_chance'] = np.where((df['type'].str.contains('rebound offensive')) |
-                                   (df['type'].str.contains('team rebound')), 1, 0)
-    # a. made basket, no foul
-    df['made basket, no foul'] = np.where((df['points'].eq(2)) & 
-                                      (df['reason_cat']!=('s.foul')) |
-                                      (df['points'].eq(3)) & 
-                                      (df['reason_cat']!='s.foul') |
-                                      df['type'].str.contains('violation:defensive goaltending'), 1, 0)
+    # df['second_chance'] = np.where((df['type'].str.contains('rebound offensive')) |
+    #                                (df['type'].str.contains('team rebound')), 1, 0)
+    # # a. made basket, no foul
+    # df['made basket, no foul'] = np.where((df['points'].eq(2)) & 
+    #                                   (df['reason_cat']!=('s.foul')) |
+    #                                   (df['points'].eq(3)) & 
+    #                                   (df['reason_cat']!='s.foul') |
+    #                                   df['type'].str.contains('violation:defensive goaltending'), 1, 0)
     
-    # b. turnover
-    df['turnover'] = np.where(df['event_type'].str.contains('turnover'), 1, 0)
+    # # b. turnover
+    # df['turnover'] = np.where(df['event_type'].str.contains('turnover'), 1, 0)
     
-    # c. missed shot rebounded by opposing team
+    # # c. missed shot rebounded by opposing team
    
-    df['def_rebound'] = np.where(df['event_type'].str.contains('rebound') & 
-                                 (df['team_t/f']== False), 1, 0)
-    df['def_rebound_cat'] = df['event_type'].astype('category').cat.codes
+    # df['def_rebound'] = np.where(df['event_type'].str.contains('rebound') & 
+    #                              (df['team_t/f']== False), 1, 0)
+    # df['def_rebound_cat'] = df['event_type'].astype('category').cat.codes
     
-    # d. 
-    # final free throw made
+    # # d. 
+    # # final free throw made
 
-    df['final ft made'] = np.where(df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 1 of 1')|
-    df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 2 of 2')|
-    df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 3 of 3'), 1, 0)
+    # df['final ft made'] = np.where(df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 1 of 1')|
+    # df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 2 of 2')|
+    # df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 3 of 3'), 1, 0)
     
-    # e. 
-    # first posession of new quarter
-    df['first poss of qt'] = np.where((df['quarter_t/f']==False) , 1, 0)
+    # # e. 
+    # # first posession of new quarter
+    # #df['first poss of qt'] = np.where((df['quarter_t/f']==False) , 1, 0)
     
     
-    # violation
-    df['off_violation'] = np.where((df['event_type'].str.contains('violation')) & 
-                              (df['team_shift']==False), 1, 0)
-    # offensive foul
-    df['off_foul'] = np.where((df['event_type'].str.contains('foul') & (df['reason_cat']==('off.foul'))), 1, 0)
+    # # violation
+    # # df['off_violation'] = np.where((df['event_type'].str.contains('violation')) & 
+    # #                           (df['team_shift']==False), 1, 0)
+    # # # offensive foul
+    # # df['off_foul'] = np.where((df['event_type'].str.contains('foul') & (df['reason_cat']==('off.foul'))), 1, 0)
     
-    # f. 
+    # # # f. 
     
 
     return df
@@ -149,35 +146,160 @@ fields = ['game_id', 'data_set', 'team', 'period', 'away_score', 'home_score',
           'remaining_time','elapsed', 'play_length', 'play_id', 'event_type', 'points', 'reason', 
           'result', 'steal', 'type', 'description']
 
-big_ass_df = pd.DataFrame()
 
 df = []
 for f in all_files:
     csv = pd.read_csv(f, usecols=fields)
     df.append(csv)
     
-big_ass_df = pd.concat(df)
+df = pd.concat(df)
+
+df = df.fillna(0)
+# create new df where team = 0
+
+############################################################################
+
+clean_df = clean_df(df)
+
+
+df = create_dummies(df)
+reset_df = create_dummies(df)
+df = reset_df
 
 
 
-clean_df = clean_df(big_ass_df)
-dummy_df = create_dummies(clean_df)
 
-dummy_df = dummy_df[dummy_df['event_type'].notna()]
-
-df = dummy_df
+df = df[df['event_type'].notna()]
 
 
-df['total'] = np.where((df['type'].str.contains('FLAGRANT')) |
+event_types = ['start of period', 'end of period', 'sub', 'timeout', 'unknown', 'ejection', 'nan']
+df['event_remove'] = np.where(df['event_type'].str.contains('|'.join(event_types)), 1, 0)
+df = df[df['event_remove']==0]
+
+
+type_remove = ['t.foul', 'c.p.foul', 'Free Throw Technical', 'Free Throw Clear Path 1 of 2',
+               'Free Throw Clear Path 2 of 2', 'Free Throw Flagrant 1 of 1', 
+               'Free Throw Flagrant 1 of 1', 'Free Throw Flagrant 1 of 2',
+               'Free Throw Flagrant 1 of 3', 'Free Throw Flagrant 2 of 2',
+               'Free Throw Flagrant 2 of 3', 'Free Throw Flagrant 3 of 3']
+
+df['type_remove'] = np.where(df['type'].str.contains('|'.join(type_remove)), 1, 0)
+df = df[df['type_remove']==0]
+
+
+
+df['points_above'] = df['points'].shift(1)
+df['points_above_t/f'] = df['points'] == df['points_above']
+
+df['points_below'] = df['points'].shift(-1)
+df['points_below_t/f'] = df['points'] == df['points_below']
+
+
+df['second_chance'] = np.where((df['type'].str.contains('rebound offensive')) |
+                                (df['type'].str.contains('team rebound')), 1, 0)
+# a. made basket, no foul
+df['made basket, no foul'] = np.where((df['points'].eq(2)) & 
+                                  (df['reason']!=('s.foul')) |
+                                  (df['points'].eq(3)) & 
+                                  (df['reason']!='s.foul') |
+                                  df['type'].str.contains('violation:defensive goaltending'), 1, 0)
+
+df['made basket, no foul'].sum()
+df['basket, no foul'] = np.where((df['points_above']==2) & ((df['type'].str.contains('s.foul'))== False) | (df['type'].str.contains('violation:defensive goaltending')), 1, 0)
+df['basket, no foul'].sum()
+
+df['2'] = np.where((df['points_above']==3) & ((df['type'].str.contains('s.foul'))==False), 1, 0)
+df['2'].sum()
+
+df['3'] = np.where((df['points_above'] ==2) & (df['type'].str.contains('s.foul')) & (df['team_above']== df['team']), 1, 0)
+df['3'].sum()
+
+
+df['4'] = np.where((df['points_above'] ==3) & (df['type'].str.contains('s.foul')) & (df['team_above']!= df['team']), 1, 0)
+df['4'].sum()
+
+df['5'] = np.where((df['type'].str.contains('violation:defensive goaltending')), 1, 0)
+df['5'].sum()
+
+
+df['type_above'] = df['type'].shift(1)
+df['6'] = np.where((df['type_above'].str.contains('violation:defensive goaltending')), 1, 0)
+df['6'].sum()
+
+df['7'] = np.where((df['event_type'].str.contains('rebound')) & (df['team_above_t/f'] == False), 1, 0)
+df['7'].sum()
+
+df['8'] = np.where((df['quarter_above_t/f']==False), 1, 0)
+df['8'].sum()
+
+
+
+# b. turnover
+df['turnover'] = np.where(df['event_type'].str.contains('turnover'), 1, 0)
+
+# remove rows that have flagrant, team rebound, clear path from type and description
+
+df['team_rebound'] = np.where((df['turnover']==1) & (df['type_below'].str.contains('team rebound')), 1, 0)
+
+
+
+df['flagrant/technical'] = np.where((df['type'].str.contains('FLAGRANT')) |
                        (df['type'].str.contains('flagrant')) |
                        (df['description'].str.contains('t.foul')) |
                        (df['description'].str.contains('T.FOUL'))|
                        (df['description'].str.contains('T.Foul')) |
                        (df['description'].str.contains('C.P.FOUL'))|
-                       (df['description'].str.contains('HANGING.TECH.FOUL')), 1, 0)
+                       (df['description'].str.contains('HANGING.TECH.FOUL'))|
+                       (df['type'].str.contains()), 1, 0)
 
-df['total'].sum()
-#1243
+searchfor = ['t.foul', 'c.p.foul', 'Free Throw Technical', 'Free Throw Clear Path 1 of 2',
+               'Free Throw Clear Path 2 of 2', 'Free Throw Flagrant 1 of 1', 
+               'Free Throw Flagrant 1 of 1', 'Free Throw Flagrant 1 of 2',
+               'Free Throw Flagrant 1 of 3', 'Free Throw Flagrant 2 of 2',
+               'Free Throw Flagrant 2 of 3', 'Free Throw Flagrant 3 of 3']
+
+df['flag'] = df['type'].str.contains('|'.join(searchfor))
+
+
+
+
+
+df = df[df['flag']==0]
+df = df[df['flagrant/technical']==0]
+df = df[df['team_rebound']==0]
+
+
+df['game_id'].nunique()
+
+# # c. missed shot rebounded by opposing team
+
+# df['def_rebound'] = np.where(df['event_type'].str.contains('rebound') & 
+#                              (df['team_t/f']== False), 1, 0)
+# df['def_rebound_cat'] = df['event_type'].astype('category').cat.codes
+
+# # d. 
+# # final free throw made
+
+# df['final ft made'] = np.where(df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 1 of 1')|
+# df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 2 of 2')|
+# df['event_type'].str.contains('free throw') & df['result'].str.contains('made') & df['type'].str.contains('Free Throw 3 of 3'), 1, 0)
+
+# # e. 
+# # first posession of new quarter
+# #df['first poss of qt'] = np.where((df['quarter_t/f']==False) , 1, 0)
+
+
+# # violation
+# # df['off_violation'] = np.where((df['event_type'].str.contains('violation')) & 
+# #                           (df['team_shift']==False), 1, 0)
+# # # offensive foul
+# # df['off_foul'] = np.where((df['event_type'].str.contains('foul') & (df['reason_cat']==('off.foul'))), 1, 0)
+
+
+
+
+
+
 
 
 df['possession change'] = np.where((df['made basket, no foul']==1) |
@@ -186,14 +308,16 @@ df['possession change'] = np.where((df['made basket, no foul']==1) |
                                    (df['final ft made']==1) |
                                    (df['off_violation']==1) |
                                    (df['off_foul']), 1, 0)
+
+df['possession change'].sum()
 #df = df.drop('possession_number', axis=1)
 
 df['new_game'] = np.where((df['game_id']!=(df.shift(-1)['game_id'])), 1, 0)
 
 # get possession number within game
 df['game_possession_number'] = df.groupby(df['new_game'].eq(1).cumsum())['possession change'].cumsum()
-df['season_possession_number'] = df.groupby('new_game')['possession change'].cumsum()
-df['season_possession_number'].nlargest()
+df['season_possession_number'] = df.groupby('new_game')(['possession change']).cumsum()
+df['season_possession_number'].max()
 # 166845
 
 
@@ -277,22 +401,18 @@ pp_FTO = total_points/total_turnover
 pp_dfrb = total_points/def_reb
 pp_offviol = total_points/off_viol
 
-game_1 = dummy_df[dummy_df['game_id'].eq('0021900001')]
 
 steve_df = pd.read_excel('/Users/hillarywolff/Desktop/game1.xlsx')
 
-game_1.compare(steve_df, keep_equal=True, keep_shape=True)
+game_1 = df[df['game_id'].eq('0021900001')]
+poss_df = steve_df['first possession of new quarter']
+hillary = game_1[['8']]
+poss_df = hillary.join(poss_df)
 
-game_1['game_id'] = (game_1['game_id']).astype(int)
+poss_df['first possession of new quarter'].sum()
+poss_df['8'].sum()
 
-merged = game_1.merge(steve_df, how='outer', left_on='game_id', right_on='game_id')
-
-
-steve_df.dtypes
-game_1.dtypes
-
-
-
+game_1['8'].sum()
 
 
 
